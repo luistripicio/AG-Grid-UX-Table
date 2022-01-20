9e70ede6833f6ea0e369fccc26be338acf4aa033
Luis Tripicio <luistripiciodev@gmail.com>
2022-01-20 10:56:32 +0800

    ag-grid install

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+function App() {
+  const rowData = [
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxter", price: 72000}
+  ];  
+  return (
+    <div className="ag-theme-alpine" style={{height: 400}}>
+      <AgGridReact
+          rowData={rowData}>
+          <AgGridColumn field="make"></AgGridColumn>
+          <AgGridColumn field="model"></AgGridColumn>
+          <AgGridColumn field="price"></AgGridColumn>
+      </AgGridReact>
+  </div>
+  );
+}
+
+export default App;
